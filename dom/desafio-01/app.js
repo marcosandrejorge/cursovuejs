b987e3cd4dfa5034b85594f2bbb70b36f0c87990
marcosbnu <marcosandre.bnu@gmail.com>
2019-04-20 17:09:19 -0300

    eventos e template dom

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,8 @@
+var desafio = new Vue({
+    el:'#desafio',
+    data:{
+        nome:'Marcos André Jorge',
+        idade:22,
+        imagem:'https://statig1.akamaized.net/bancodeimagens/8y/fy/5m/8yfy5m6q3hoq0jror74husliw.jpg'
+    }
+})
