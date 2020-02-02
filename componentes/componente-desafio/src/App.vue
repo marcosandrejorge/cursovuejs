dca3f5d388fb5fcc66a321f027595e89b86da1f9
Marcos André Jorge <marcosandre.bnu@gmail.com>
2020-02-02 15:17:10 -0300

    Componentes .vue

---
+++ componentes/componente-desafio/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+	<v-app>
+		<app-menu :config-menu="itensMenu">
+		</app-menu>
+
+		<v-toolbar app>
+			<v-toolbar-title class="headline text-uppercase">
+				<span>Desafio</span>
+				<span class="font-weight-light">Componente</span>
+			</v-toolbar-title>
+		</v-toolbar>
+
+		<v-content>
+			<v-carousel>
+				<v-carousel-item
+					v-for="(item,i) in fotos"
+					:key="i"
+					:src="item.src"></v-carousel-item>
+			</v-carousel>
+		</v-content>
+
+		<v-footer class="pa-3" app>
+			<v-spacer></v-spacer>
+			<div>Curso Vue &copy; {{ new Date().getFullYear() }}</div>
+		</v-footer>
+	</v-app>
+</template>
+
+<script>
+import Menu from './Menu.vue'
+
+export default {
+	components: {
+		'app-menu': Menu
+	},
+	data() {
+		return {
+			itensMenu: [
+				{titulo: 'Início', icon: 'dashboard'},
+				{titulo: 'Sobre', icon: 'question_answer'}
+			],
+			fotos: [
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/bird.jpg'},
+				{src: 'https://cdn.vuetifyjs.com/images/carousel/planet.jpg'}
+			]
+		}
+	}
+}
+</script>
