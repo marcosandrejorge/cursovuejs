08090696c98e5c3213ac4a314e53d32ba11d1f18
Marcos André Jorge <marcosandre.bnu@gmail.com>
2020-02-09 15:36:09 -0300

    curso vue componentes avançados

---
+++ componentes-avancados/citacoes/src/components/Citacoes.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="citacoes">
+        <span>
+            <button @click="numero--">&lt;</button>
+            <button @click="numero++">&gt;</button>
+        </span>
+        {{ numero }} {{ indice }}
+        <Citacao>
+            <p> {{ citacoes[indice].texto }} </p>
+        </Citacao>
+    </div>
+</template>
+
+<script>
+import Citacao from './Citacao.vue';
+
+export default {
+    components:{
+        Citacao
+    },
+    data() {
+        return {
+            numero: 0,
+            citacoes: [{
+                fonte: 'Jornal do Empreendedor',
+                texto: 'Lembre-se sempre que você é absolutamente único. Assim como todos os outros.',
+                autor: 'Margaret Mead'
+            }, { 
+                fonte: 'Frases de Mãe',
+                texto: 'Isso não é um quarto, é um chiqueiro.',
+                autor: 'Roberta'
+            }, {
+                fonte: 'Frases de Pai',
+                texto: 'Vou contar até 3! 1, 2, 2...',
+                autor: 'Gustavo'
+            }]
+        }
+    },
+    computed: {
+        indice() {
+            return Math.abs(this.numero % 3)
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .citacoes {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
